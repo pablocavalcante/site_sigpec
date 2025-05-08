3585c0f57b6f0bef80ad6432483b1dc56586a800
pablocavalcante <pablocavalcante@prodam.sp.gov.br>
2025-05-08 14:51:07 -0300

    Ajuste tecnico 08.05.2025

--- script.js
+++ script.js
@@ -20,3 +20,33 @@ let tamanhoFonte = 100;
       atualizarFonte();
     }
   }
+
+  // Botão de ajuda
+  const botaoAjuda = document.getElementById('botao-ajuda');
+  const caixaAjuda = document.getElementById('caixa-ajuda');
+  const fecharAjuda = document.getElementById('fechar-ajuda');
+  const botaoMeAjuda = document.getElementById('botao-ajuda');
+
+  // botaoAjuda.addEventListener('click', () => {
+  //   caixaAjuda.style.display = 'block';
+  // });
+
+  fecharAjuda.addEventListener('click', () => {
+    caixaAjuda.style.display = 'none';
+  });
+
+  botaoAjuda.addEventListener('click', () => {
+    if(caixaAjuda.style.display === 'none' || caixaAjuda.style.display === ''){
+      caixaAjuda.style.display = 'block';
+    } else {
+      caixaAjuda.style.display = 'none';
+    }
+  });
+
+
+
+  document.querySelector('.form-ajuda').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Mensagem enviada! Entraremos em contato.');
+    caixaAjuda.style.display = 'none';
+  });
